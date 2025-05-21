620ed6b81c7762263ab8b31058cbde0e44ff2a47
sourabh jhamnani <jhamnanisourabh2@gmail.com>
2025-05-21 17:11:33 +0530

    cfm

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -3,7 +3,9 @@ const { StatusCodes } = require("http-status-codes")
 function errorHandler(err, req, res, next) {
   if (err) {
     if (err.name === "JsonWebTokenError")
-      err.statusCode = StatusCodes.BAD_REQUEST
+      err.statusCode = StatusCodes.FORBIDDEN
+    else if (err.name === "TokenExpiredError")
+      err.statusCode = StatusCodes.UNAUTHORIZED
     console.log(err)
     res.status(err.statusCode || 500).send(err.message)
   }
